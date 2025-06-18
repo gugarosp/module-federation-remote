aae978768071773c6c1876446d5a8affac8881f6
Gustavo Pereira <gugarosp@gmail.com>
2025-06-18 15:36:27 -0300

    Zephyr configs

--- module-federation-remote/vite.config.js
+++ module-federation-remote/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
+import { withZephyr } from 'vite-plugin-zephyr';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
+        withZephyr(),
         federation({
             name: 'remote_app',
             filename: 'remoteEntry.js',
